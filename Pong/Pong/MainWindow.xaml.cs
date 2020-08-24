588d816142a1bfa0d3a2610a6101a8e3f629bea5
Lilla Kertesz <lil.kertesz@gmail.com>
2020-08-24 13:45:58 +0200

    basic classes

--- Pong/Pong/MainWindow.xaml.cs
+++ Pong/Pong/MainWindow.xaml.cs
@@ -24,5 +24,6 @@ namespace Pong
         {
             InitializeComponent();
         }
+
     }
 }
---
+++ Pong/Pong/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong
+{
+    class Paddle
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public Position  { get; set; }
+
+    }
+}
