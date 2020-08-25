ee7ca5c75da44c8f09ac244a0bba547ea226e411
alexseres <lilaalex95@gmail.com>
2020-08-25 04:09:10 -0700

    paddle rewritten and having put on screen

--- Pong/Pong/MainWindow.xaml.cs
+++ Pong/Pong/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,37 +22,70 @@ namespace Pong
     public partial class MainWindow : Window
     {
 
-        Paddle paddle = new Paddle { Height = 30, Width = 200, Pos = new Position { X = 500, Y = 300 } };
+        //Paddle paddle = new Paddle { Height = 30, Width = 200, Pos = new Position { X = 500, Y = 300 } };
+        Paddle paddle = new Paddle{Position = new Point(500,300), Height=30, Width = 200};
+        private SolidColorBrush paddleColor = Brushes.Gold;
+        
 
         public MainWindow()
         {
             InitializeComponent();
-
             this.DataContext = paddle;
+            paddle.UiElement = new Rectangle()
+            {
+                Width = paddle.Width,
+                Height = paddle.Height,
+                Fill = paddleColor
+            };
+            
+            
             double leftAlign = (GameArea.Width - paddle.Width) / 2;
             double topAlign = GameArea.Height - paddle.Height;
-
-
-            Rectangle paddleUI = new Rectangle { Height = paddle.Height, Width = paddle.Width, Fill = Brushes.Black };
-            GameArea.Children.Add(paddleUI);
-            Canvas.SetLeft(paddleUI, leftAlign);
-            Canvas.SetTop(paddleUI, topAlign);
-
-
-
-     
+            Canvas.SetTop(paddle.UiElement, topAlign);
+            Canvas.SetLeft(paddle.UiElement, leftAlign);
+            GameArea.Children.Add(paddle.UiElement);
             
+
+
+            //this.DataContext = paddle;
+            //Rect paddleUI = new Rect
+            //{ 
+            //    Height = paddle.Height,
+            //    Width = paddle.Width,
+           //     X = paddle.Pos.X,
+           //     Y = paddle.Pos.Y
+           //};
+            //drawPaddle(paddleUI);
+            //movePaddle(paddleUI);
         }
 
-        
+ //       private void drawPaddle(Rect paddleUI){
+ //           double leftAlign = (GameArea.Width - paddle.Width) / 2;
+//            double topAlign = GameArea.Height - paddle.Height;
+ //           GameArea.Children.Add(paddleUI);
+ //           Canvas.SetLeft(paddleUI, leftAlign);
+ //           Canvas.SetTop(paddleUI, topAlign);
+            
+ //       }
 
-        private void Canvas_KeyDown(object sender, KeyEventArgs e)
+        private void movePaddle(Rect paddleUI)
         {
-            //switch (e.Key)
-            //{
-            //    case Key.Left:
-            //        break;
-            //}
+
+
         }
+
+
+
+        //private void Canvas_KeyDown(object sender, KeyEventArgs e)
+        //{
+            //switch (e.key)
+            //{
+            //    //case key.left:
+            //    //    //paddleUI.
+            //    //    break;
+            //    //case key.Right:
+            //    //    break;
+            //}
+        //}
     }
 }
--- Pong/Pong/Paddle.cs
+++ Pong/Pong/Paddle.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Shapes;
 
 namespace Pong
 {
     class Paddle
     {
+       
+        public UIElement UiElement{get;set;}
+        public Point Position { get; set; }
+
         public int Height { get; set; }
         public int Width { get; set; }
+        //public Position Pos { get; set; }
 
-        public Position Pos { get; set; }
 
-        
 
     }
 }
